a738e5d0252195cbf49dd8c56ca2ca843588c7f4
iKurisu <chris.epic01@gmail.com>
2020-02-17 11:38:01 +0100

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint", "react"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+}
