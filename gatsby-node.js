f35ee148b512db694608152f4362a812579ffe4b
iKurisu <chris.epic01@gmail.com>
2020-02-19 10:05:56 +0100

    create pages dynamically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,24 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const articles = await graphql(`
+    query {
+      allStrapiArticle {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  articles.data.allStrapiArticle.edges.forEach(({ node }) => {
+    createPage({
+      path: node.slug,
+      component: path.resolve("./src/components/article.tsx"),
+      context: { slug: node.slug },
+    })
+  })
+}
