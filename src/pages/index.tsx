465d9240aba2d6943063b40104efa0838be4c287
iKurisu <chris.epic01@gmail.com>
2020-02-20 08:35:59 +0100

    clean up generated files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import Markdown from "react-markdown"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+interface Props {
+  data: {
+    allStrapiArticle: {
+      edges: Array<{
+        node: { title: string; image: { publicURL: string }; content: string }
+      }>
+    }
+  }
+}
+
+const IndexPage = ({ data }: Props) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>{data.allStrapiArticle.edges[0].node.title}</h1>
+    <img src={data.allStrapiArticle.edges[0].node.image.publicURL} />
+    <Markdown source={data.allStrapiArticle.edges[0].node.content} />
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query Articles {
+    allStrapiArticle {
+      edges {
+        node {
+          title
+          image {
+            publicURL
+          }
+          content
+        }
+      }
+    }
+  }
+`
