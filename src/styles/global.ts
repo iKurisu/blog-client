eb78d5fe69e900f47a43efbadbc9ee3ceb09924b
iKurisu <chris.epic01@gmail.com>
2020-02-22 10:33:08 +0100

    add extra global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,14 +6,16 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-
+  
   body {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     word-wrap: break-word;
     font-kerning: normal;
     font-feature-settings: "kern", "liga", "clig", "calt";
+    text-rendering: optimizeLegibility;
     background-color: #fbfbfb;
+    line-height: 1;
   }
 
   li {
