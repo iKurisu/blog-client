67165b5cf8d41f5814867569717357c696b448a8
iKurisu <chris.epic01@gmail.com>
2020-02-27 08:15:18 +0100

    polish article margin bottom

--- src/components/story/article.styled.ts
+++ src/components/story/article.styled.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const StyledArticle = styled.article`
-  margin-bottom: 96px;
+  margin-bottom: 80px;
 `
 
 export const HeadingWrapper = styled.div`
